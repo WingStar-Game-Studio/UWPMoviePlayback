05c7e9f526d51cbe9cd885057b7f4df697fd0a5e
mahorigahama <mahorigahama@noreply.github.com>
2015-12-10 22:35:57 +0900

    1st commit.

---
+++ UWPMoviePlayback/MainPage.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.ComponentModel;
+using Windows.Media.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPMoviePlayback
+{
+    public class PlayerProps : INotifyPropertyChanged
+    {
+        private void RaisePropChgEvent(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+        long duration;
+        public long Duration
+        {
+            get
+            {
+                return duration;
+            }
+            set
+            {
+                if (duration == value)
+                {
+                    return;
+                }
+                duration = value;
+                RaisePropChgEvent("Duration");
+            }
+        }
+
+        long position;
+        public long Position
+        {
+            get
+            {
+                return position;
+            }
+            set
+            {
+                if (position == value)
+                {
+                    return;
+                }
+                position = value;
+                RaisePropChgEvent("Position");
+            }
+        }
+
+        double moviePos;
+        public double MoviePos
+        {
+            get
+            {
+                return moviePos;
+            }
+            set
+            {
+                if (moviePos == value)
+                {
+                    return;
+                }
+                moviePos = value;
+                RaisePropChgEvent("MoviePos");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    };
+
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        PlayerProps playerProps;
+        private DispatcherTimer timer;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(0.1);
+            timer.Tick += timer_Tick;
+
+            this.seekBar.IsEnabled = false;
+            playerProps = new PlayerProps();
+            this.DataContext = playerProps;
+        }
+
+        private async void playBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri("ms-appx:///Assets/fishes.wmv");
+            var file = await Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(uri);
+            if (file != null)
+            {
+                this.mediaElement.SetPlaybackSource(MediaSource.CreateFromStorageFile(file));
+                this.mediaElement.Play();
+            }
+        }
+
+        private void stopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.mediaElement.Stop();
+            this.timer.Stop();
+        }
+
+        private void mediaElement_Opened(object sender, RoutedEventArgs e)
+        {
+            this.playerProps.Position = this.mediaElement.Position.Ticks;
+            this.playerProps.Duration = this.mediaElement.NaturalDuration.TimeSpan.Ticks;
+            this.timer.Start();
+            this.seekBar.IsEnabled = true;
+        }
+
+        void timer_Tick(object sender, object e)
+        {
+            this.playerProps.Position = this.mediaElement.Position.Ticks;
+            this.playerProps.MoviePos = (this.playerProps.Position * 1000 / this.playerProps.Duration) / 1000.0;
+        }
+
+        private void SeekBar_ValueChanged(object sender, SeekBarValueChangedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("ValueChanged : " + e.Value);
+            long ticks = (long)(e.Value * playerProps.Duration);
+            this.mediaElement.Position = TimeSpan.FromTicks(ticks);
+        }
+    }
+}
---
+++ UWPMoviePlayback/SeekBar.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.ComponentModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UWPMoviePlayback
+{
+    public class SeekBarValueChangedEventArgs : System.EventArgs
+    {
+        private double value;
+
+        public SeekBarValueChangedEventArgs(double value)
+        {
+            this.value = value;
+        }
+
+        public double Value
+        {
+            get
+            {
+                return this.value;
+            }
+        }
+    }
+
+    public class SeekBarProps : INotifyPropertyChanged
+    {
+        double hPos;
+        public double HPos
+        {
+            get
+            {
+                return hPos;
+            }
+            set
+            {
+                if (hPos == value)
+                {
+                    return;
+                }
+                hPos = value;
+                if (this.PropertyChanged != null)
+                {
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("HPos"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public sealed partial class SeekBar : UserControl
+    {
+        private SeekBarProps seekBarProps;
+        private double thumbX = 0;
+        private bool dragEntered = false;
+        private double value = 0;
+
+        public delegate void ValueChangedDelegate(object sender, SeekBarValueChangedEventArgs e);
+        public event ValueChangedDelegate ValueChanged;
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+        public static readonly DependencyProperty ValueProperty =
+          DependencyProperty.Register(
+            "Value", typeof(double), typeof(SeekBar),
+            new PropertyMetadata(null, new PropertyChangedCallback(OnValueChanged))
+          );
+
+        private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var thisInstance = d as SeekBar;
+            thisInstance.MoveThumb((double)e.NewValue);
+        }
+
+        public SeekBar()
+        {
+            this.InitializeComponent();
+            seekBarProps = new SeekBarProps();
+            this.SliderBase.DataContext = seekBarProps;
+            this.value = 0;
+        }
+
+        public void MoveThumb(double value)
+        {
+            var width = this.SliderBase.ActualWidth - this.PositionThumb.ActualWidth;
+            this.value = value;
+            if (this.dragEntered == false)
+            {
+                seekBarProps.HPos = width * value;
+            }
+        }
+
+        private void PositionThumb_DragDelta(object sender, Windows.UI.Xaml.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            var width = this.SliderBase.ActualWidth - this.PositionThumb.ActualWidth;
+            thumbX += e.HorizontalChange;
+            seekBarProps.HPos = Math.Min(width, Math.Max(0, thumbX));
+        }
+
+        private void PositionThumb_DragStarted(object sender, Windows.UI.Xaml.Controls.Primitives.DragStartedEventArgs e)
+        {
+            thumbX = seekBarProps.HPos;
+            dragEntered = true;
+        }
+
+        private void PositionThumb_DragCompleted(object sender, Windows.UI.Xaml.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            var width = this.SliderBase.ActualWidth - this.PositionThumb.ActualWidth;
+            dragEntered = false;
+            if (this.ValueChanged != null)
+            {
+                this.value= seekBarProps.HPos / width;
+                this.ValueChanged(this, new SeekBarValueChangedEventArgs(this.value));
+            }
+        }
+
+        private void PositionThumb_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var width = this.SliderBase.ActualWidth - this.PositionThumb.ActualWidth;
+            if (this.dragEntered == false)
+            {
+                seekBarProps.HPos = width * this.value;
+            }
+        }
+    }
+}
